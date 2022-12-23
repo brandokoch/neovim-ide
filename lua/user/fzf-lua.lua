acfff504bb1d3351e24b9c3750d2e9edd042b6c3
brando <brando>
2022-12-23 13:49:23 +0100

    change window size for fzf

--- lua/user/fzf-lua.lua
+++ lua/user/fzf-lua.lua
@@ -20,10 +20,10 @@ fzflua.setup({
                                         -- "aboveleft vnew   : split left
     -- Only valid when using a float window
     -- (i.e. when 'split' is not defined, default)
-    height           = 0.85,            -- window height
-    width            = 0.80,            -- window width
-    row              = 0.35,            -- window row position (0=top, 1=bottom)
-    col              = 0.50,            -- window col position (0=left, 1=right)
+    height           = 1.0,            -- window height
+    width            = 1.0,            -- window width
+    row              = 0.1,            -- window row position (0=top, 1=bottom)
+    col              = 0.1,            -- window col position (0=left, 1=right)
     -- border argument passthrough to nvim_open_win(), also used
     -- to manually draw the border characters around the preview
     -- window, can be set to 'false' to remove all borders or to
@@ -58,9 +58,9 @@ fzflua.setup({
                                         -- native fzf previewers (bat/cat/git/etc)
       wrap           = 'nowrap',        -- wrap|nowrap
       hidden         = 'nohidden',      -- hidden|nohidden
-      vertical       = 'down:45%',      -- up|down:size
+      vertical       = 'down:60%',      -- up|down:size
       horizontal     = 'right:60%',     -- right|left:size
-      layout         = 'flex',          -- horizontal|vertical|flex
+      layout         = 'vertical',          -- horizontal|vertical|flex
       flip_columns   = 120,             -- #cols to switch to horizontal on flex
       -- Only used with the builtin previewer:
       title          = true,            -- preview border title (file/buf)?
