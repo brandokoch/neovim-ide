06c980db5a7af9a4f207628f3b119d0809aaf2a3
brando <brando>
2023-01-06 17:20:11 +0100

    update

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -24,6 +24,32 @@ dap.adapters.python = {
   -- args = { '-m', 'debugpy.adapter' };
 }
 
+dap.adapters.codelldb= {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = '/home/bkoch/.local/share/nvim/mason/bin/codelldb',
+    args = {"--port", "${port}"},
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+
 -- local continue = function()
 -- 	if vim.fn.filereadable('.vscode/launch.json') then
 -- 		require('dap.ext.vscode').load_launchjs()
